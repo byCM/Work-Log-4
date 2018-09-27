8185141b51d8ab2bb5124e6a0f33f5fc47973ddf
byCM <43051181+byCM@users.noreply.github.com>
2018-09-26 19:26:57 -0800

    Create entry.py

---
+++ entry.py
@@ -0,0 +1,25 @@
+from peewee import *
+
+import datetime
+
+db = SqliteDatabase('entries.db')
+
+class Entry(Model):
+    employee_name = CharField(max_length=30)
+    date = DateTimeField(default=datetime.datetime.now)
+    task_name = CharField(max_length=50)
+    minutes = IntegerField(default=0)
+    notes = CharField(max_length=100)
+    
+    
+    
+    class Meta:
+        databse = db
+
+
+def initialize():
+    db.connect()
+    db.create_tables([Entry], safe=True)
+    
+        
+
