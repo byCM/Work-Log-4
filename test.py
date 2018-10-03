bc40493c8b75a22c0cadb82a7634a5ac29dd254e
byCM <43051181+byCM@users.noreply.github.com>
2018-10-02 19:45:28 -0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,78 @@
+import unittest
+import unittest.mock as mock
+
+from playhouse.test_utils import test_database
+from datetime import datetime
+from peewee import *
+
+import work_log
+from entry import Entry
+
+TEST = {
+    "employee_name": "Mason",
+    "task_name": "Cleaning",
+    "minutes": 40,
+    "notes": "Some notes.",
+    "date": "2012-12-12"
+}
+
+TEST2 = {
+    "emplyee_name": "Carter",
+    "task_name": "Mopping",
+    "minutes": 30,
+    "notes": "Some more notes",
+    "date": "2018-10-12"
+}
+
+class LogTest(unittest.TestCase):
+
+    @staticmethod
+    def entry_creator():
+        worklog.Entry.create(
+            employee_name=TEST["employee_name"],
+            task_name=TEST["task_name"],
+            date=TEST["date"],
+            minutes=TEST["minutes"],
+            task_notes=TEST["task_notes"])
+
+        worklog.Entry.create(
+            employee_name=TEST2["employee_name"],
+            task_name=TEST2["task_name"],
+            date=TEST2["date"],
+            minutes=TEST2["minutes"],
+            notes=TEST2["notes"])
+        
+    def test_employees_name(self):
+        with unittest.mock.patch('builtins.input', side_effect = ["", "", "alex"]):
+            assert work_log.employees_name() == "alex"
+
+    def test_time_spent(self):
+        with unittest.mock.patch('builtins.input', side_effect = ["", "", 10]):
+            assert work_log.time_spent() == 10
+
+    def test_task_title(self):
+        with unittest.mock.patch('builtins.input', side_effect = ["", "", "proj"]):
+            assert work_log.task_title() == "proj"            
+
+    def test_task_date(self):
+        with unittest.mock.patch('builtins.input', side_effect = ["", "", "2018-12-12"]):
+            assert work_log.task_date() == "2018-12-12"  
+            
+    def test_add_entry(self):
+        with unittest.mock.patch('builtins.input',
+            side_effect=["2012-12-12", "Name", "Cleaning", 45,
+            "Very Clean", "y", ""]
+            , return_value=TEST):
+            assert work_log.add_entry()["task_name"] == TEST["task_name"]
+
+        with unittest.mock.patch('builtins.input',
+            side_effect=["2016-12-25", "Name", "Cleaning", 45,
+            "Very Clean", "n", ""]
+            , return_value=TEST):
+            assert work_log.add_entry() == None
+
+
+
+        
+if __name__ == '__main__':
+    unittest.main()
